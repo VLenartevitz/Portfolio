d5f35a8248d861c47d8ec6640804fc0d5602817f
Vinicius Lenartevitz Candeira <viniciuslenartevitz@gmail.com>
2024-11-10 14:40:34 -0300

    projeto de base

---
+++ assets/js/actionButton.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Ajusta para garantir que o site comece no topo ao recarregar
+    if (window.location.hash !== '#inicio') {
+        window.location.hash = '#inicio';
+    }
+
+    const backToTopButton = document.getElementById('backToTop');
+    const whatsappButton = document.getElementById('whatsappButton');
+
+    // Função para exibir ou esconder os botões ao rolar a página
+    function toggleButtons() {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition > 100) {
+            backToTopButton.style.display = 'flex';
+            whatsappButton.style.display = 'flex';
+            setTimeout(() => {
+                backToTopButton.style.opacity = '1';
+                whatsappButton.style.opacity = '1';
+            }, 10);
+        } else {
+            backToTopButton.style.opacity = '0';
+            whatsappButton.style.opacity = '0';
+            setTimeout(() => {
+                backToTopButton.style.display = 'none';
+                whatsappButton.style.display = 'none';
+            }, 500);
+        }
+    }
+
+    // Adiciona o evento de scroll para exibir ou esconder os botões
+    document.addEventListener('scroll', toggleButtons);
+
+    // Ação ao clicar no botão "Voltar ao Topo"
+    backToTopButton.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    // Ação ao clicar no botão do WhatsApp
+    whatsappButton.addEventListener('click', function () {
+        window.location.href = 'https://wa.me/5599999999999';
+    });
+});
